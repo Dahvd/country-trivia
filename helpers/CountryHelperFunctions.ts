90c0d8d210ffb406d133e1a5859afe0d5311dbf4
David Oligney <davidoligney@gmail.com>
2024-12-14 12:00:00 -0700

    Separate it out

---
+++ helpers/CountryHelperFunctions.ts
@@ -0,0 +1,29 @@
+import { Country } from "@/types/global";
+
+//function to shuffle an array based on Fisher-Yates Shuffle:
+export function shuffle(array: any[]): void {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+export function getRandomCountryGuesses(countryData: Country[], countryIndex: number) {
+  console.log('country data in randomcountry function: ', countryData);
+
+  let countriesToBeGuessed = [countryData[countryIndex]];  // Start with the current country index
+
+  // Remove the current index from the pool of available indices
+  let shortenedCountryList = countryData.filter(item => item.name.common !== countryData[countryIndex].name.common);
+
+  // Select 3 additional random countries (ensuring no duplicates)
+  while (countriesToBeGuessed.length < 4) {
+      const randomIndex = Math.floor(Math.random() * shortenedCountryList.length);
+      const selectedCountry = shortenedCountryList[randomIndex];
+      countriesToBeGuessed.push(selectedCountry);
+      shortenedCountryList.splice(randomIndex, 1);
+  }
+
+  shuffle(countriesToBeGuessed);
+  return countriesToBeGuessed;
+}
